d6efdea54cfdf7337162e41e85f30f20485dfc9a
Matt Hirsch <matt@fathomoptics.com>
2021-09-25 21:38:13 -0400

    handle strings separated by :

--- index.js
+++ index.js
@@ -20,11 +20,17 @@ module.exports.vipsheader = async function(file) {
   a.shift(); // remove 1st and last
   a.forEach( (e,i,a) => {
     var k = e.split(':');
-    var s = (k[0] === 'vips-loader' || k[0] === 'xml-header'); // make vips-loader a string
+    var s = (k[0] === 'vips-loader' || k[0] === 'xml-header' || k[0] === 'jpeg-chroma-subsample'); // make vips-loader a string
     var t = (k[0] === 'format' || k[0] === 'coding' || k[0] === 'interpretation');
     k[0] = '"' + k[0] + '"';
     if(t) k[1] = k[1].split('-').pop(); // remove stuff before the -
-    if(s) k[1] = '"' + k[1].substring(1, k[1].length) + '"';
+    if(s) {
+      kp = [...k];
+      kp.shift();
+      let str = kp.join(':');
+      k[1] = '"' + str.substring(1, str.length) + '"';
+      k = k.slice(0, 2);
+    }
     k[1] = k[1].trim();
     if(!isNumeric(k[1]) && k[1][0] !== '"')
       k[1]='"'+k[1]+'"';
